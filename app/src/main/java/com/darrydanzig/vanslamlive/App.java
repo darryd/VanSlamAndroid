c8ec5d1f97d71c941c8df42d64641d8a355bdc36
Darry Danzig <darry.d@gmail.com>
2016-04-06 12:54:34 -0700

    Adding back App class

---
+++ app/src/main/java/com/darrydanzig/vanslamlive/App.java
@@ -0,0 +1,41 @@
+package com.darrydanzig.vanslamlive;
+
+import android.app.Application;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.lang.reflect.Modifier;
+
+
+public class App extends Application {
+
+    private static App singleton;
+    private Gson gson;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        singleton = this;
+
+        initGson();
+    }
+
+    private void initGson() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.excludeFieldsWithModifiers(Modifier.TRANSIENT);
+        gsonBuilder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
+
+        gson = gsonBuilder.create();
+    }
+
+    public Gson getGson() {
+        return gson;
+    }
+
+    public static App getInstance() {
+        return singleton;
+    }
+}
